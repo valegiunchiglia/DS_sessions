bda9cc7cb74df02643748586a50d28d3c4b99b34
valegiunchiglia <38138770+valegiunchiglia@users.noreply.github.com>
2021-06-01 19:08:06 +0100

    Update job_cpu.sh

--- Lecture_Material/0206_Linux_and_HPC/job_cpu.sh
+++ Lecture_Material/0206_Linux_and_HPC/job_cpu.sh
@@ -1,5 +1,5 @@
 #PBS -lwalltime=24:00:00
-#PBS -lselect=1:ncpus=16:mem=96gb:ngpus=4:gpu_type=RTX6000
+#PBS -lselect=1:ncpus=32:mem=124gb
 
 module load anaconda3/personal
 source activate test1
@@ -8,3 +8,4 @@ python3 $HOME/test_script.py
 
 mkdir $WORK/$PBS_JOBID
 cp * $WORK/$PBS_JOBID
+
